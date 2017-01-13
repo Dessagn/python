07cc8cb11cedf9dc29c9a94c12b36753b42241d8
Temesgen Boja <temesgen.dessalegn@gmail.com>
2017-01-13 07:00:50 +0200

    file01.py first commit

---
+++ files01.py
@@ -0,0 +1,33 @@
+def space():
+	print ""
+	print ("********************* OUT PUT **********************")
+	print ""
+
+space()
+print ""
+
+print ("Python files Intro!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print ""
+space()
+
+
